1baf22f43c9c5bd1a23d83a46fc33105bfe13e4b
Huda Febrianto Nurrohman <febriantohuda@gmail.com>
2025-05-01 21:59:27 +0700

    🧑‍💻 Auto-completion first rather than auto-import based on module name

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,6 @@
-import os
-import importlib
-import pkgutil
+# import os
+# import importlib
+# import pkgutil
 from datetime import datetime
 from pydantic import ConfigDict
 from sqlmodel import Field, SQLModel
@@ -34,29 +34,28 @@ class BaseModelUpdate(BaseModel):
     date_updated: datetime = Field(default_factory=utcnow)
 
 
-# from app.models.main import *
-# from app.models.user import *
-# from app.models.item_category import *
-# from app.models.item_unit import *
-# from app.models.item import *
+from app.models.main import *
+from app.models.user import *
+from app.models.item_category import *
+from app.models.item_unit import *
+from app.models.item import *
 
-# --- below same with above --- #
+# --- below same with above but auto-completion is not supported--- #
+# # get the current directory name
+# package_dir = os.path.dirname(__file__)
+# package_name = __name__
 
-# get the current directory name
-package_dir = os.path.dirname(__file__)
-package_name = __name__
+# __all__ = []
 
-__all__ = []
+# # auto-import all modules on this directory
+# # loop every module (.py) inside this directory (except __init__.py)
+# for _, module_name, is_pkg in pkgutil.iter_modules([package_dir]):
+#     if not is_pkg:
+#         # Impor modulnya, misalnya models.item
+#         module = importlib.import_module(f".{module_name}", package=package_name)
 
-# auto-import all modules on this directory
-# loop every module (.py) inside this directory (except __init__.py)
-for _, module_name, is_pkg in pkgutil.iter_modules([package_dir]):
-    if not is_pkg:
-        # Impor modulnya, misalnya models.item
-        module = importlib.import_module(f".{module_name}", package=package_name)
-
-        # Tambahkan semua objek yang tidak diawali _
-        for attr in dir(module):
-            if not attr.startswith("_"):
-                globals()[attr] = getattr(module, attr)
-                __all__.append(attr)
+#         # Tambahkan semua objek yang tidak diawali _
+#         for attr in dir(module):
+#             if not attr.startswith("_"):
+#                 globals()[attr] = getattr(module, attr)
+#                 __all__.append(attr)
