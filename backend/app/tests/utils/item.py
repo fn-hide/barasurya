3b2a13d7d03bd4d322f9bfedfe51edbb7716fc65
Huda Febrianto Nurrohman <febriantohuda@gmail.com>
2025-05-03 09:38:58 +0700

    🔨 Add and update utility to create item_category, item_unit, and item

--- backend/app/tests/utils/item.py
+++ backend/app/tests/utils/item.py
@@ -3,6 +3,8 @@ from sqlmodel import Session
 from app import crud
 from app.models import Item, ItemCreate
 from app.tests.utils.user import create_random_user
+from app.tests.utils.item_category import create_random_item_category
+from app.tests.utils.item_unit import create_random_item_unit
 from app.tests.utils.utils import random_lower_string
 
 
@@ -10,7 +12,16 @@ def create_random_item(db: Session) -> Item:
     user = create_random_user(db)
     owner_id = user.id
     assert owner_id is not None
+    
+    item_category = create_random_item_category(db)
+    item_category_id = item_category.id
+    assert item_category_id is not None
+    
+    item_unit = create_random_item_unit(db)
+    item_unit_id = item_unit.id
+    assert item_unit_id is not None
+    
     title = random_lower_string()
     description = random_lower_string()
-    item_in = ItemCreate(title=title, description=description)
+    item_in = ItemCreate(title=title, description=description, item_category_id=item_category_id, item_unit_id=item_unit_id)
     return crud.create_item(session=db, item_in=item_in, owner_id=owner_id)
---
+++ backend/app/tests/utils/item_unit.py
@@ -0,0 +1,16 @@
+from sqlmodel import Session
+
+from app import crud
+from app.models import ItemUnit, ItemUnitCreate
+from app.tests.utils.user import create_random_user
+from app.tests.utils.utils import random_lower_string
+
+
+def create_random_item_unit(db: Session) -> ItemUnit:
+    user = create_random_user(db)
+    owner_id = user.id
+    assert owner_id is not None
+    name = random_lower_string()
+    description = random_lower_string()
+    item_unit_in = ItemUnitCreate(name=name, description=description)
+    return crud.create_item_unit(session=db, item_unit_in=item_unit_in, owner_id=owner_id)
