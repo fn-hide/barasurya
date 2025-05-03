1175d0dcd1760d9acb407ca27e694f87056d9cab
Huda Febrianto Nurrohman <febriantohuda@gmail.com>
2025-05-03 09:39:17 +0700

    🔨 Add and update utility to create item_category, item_unit, and item

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -4,7 +4,7 @@ from typing import Any
 from sqlmodel import Session, select
 
 from app.core.security import get_password_hash, verify_password
-from app.models import Item, ItemCreate, User, UserCreate, UserUpdate, BaseModelUpdate
+from app.models import Item, ItemCreate, User, UserCreate, UserUpdate, BaseModelUpdate, ItemCategory, ItemCategoryCreate, ItemUnit, ItemUnitCreate
 
 
 def create_user(*, session: Session, user_create: UserCreate) -> User:
@@ -45,6 +45,22 @@ def authenticate(*, session: Session, email: str, password: str) -> User | None:
     if not verify_password(password, db_user.hashed_password):
         return None
     return db_user
+
+
+def create_item_category(*, session: Session, item_category_in: ItemCategoryCreate, owner_id: uuid.UUID) -> ItemCategory:
+    db_item_category = ItemCategory.model_validate(item_category_in, update={"owner_id": owner_id})
+    session.add(db_item_category)
+    session.commit()
+    session.refresh(db_item_category)
+    return db_item_category
+
+
+def create_item_unit(*, session: Session, item_unit_in: ItemUnitCreate, owner_id: uuid.UUID) -> ItemUnit:
+    db_item_unit = ItemUnit.model_validate(item_unit_in, update={"owner_id": owner_id})
+    session.add(db_item_unit)
+    session.commit()
+    session.refresh(db_item_unit)
+    return db_item_unit
 
 
 def create_item(*, session: Session, item_in: ItemCreate, owner_id: uuid.UUID) -> Item:
