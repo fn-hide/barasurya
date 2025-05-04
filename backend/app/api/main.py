09d7b7855046643a680301a0bbaa9525af76c95d
Huda Febrianto Nurrohman <febriantohuda@gmail.com>
2025-05-04 09:15:16 +0700

    ✨ Add new customer types api

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.routes import items, item_categories, item_units, stores, suppliers, login, private, users, utils
+from app.api.routes import items, item_categories, item_units, stores, suppliers, customer_types, login, private, users, utils
 from app.core.config import settings
 
 api_router = APIRouter()
@@ -12,6 +12,7 @@ api_router.include_router(item_categories.router)
 api_router.include_router(item_units.router)
 api_router.include_router(stores.router)
 api_router.include_router(suppliers.router)
+api_router.include_router(customer_types.router)
 
 
 if settings.ENVIRONMENT == "local":
