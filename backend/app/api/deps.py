b80994bdefcd99fa664001a9759079ca44d50e50
Huda Febrianto Nurrohman <febriantohuda@gmail.com>
2025-05-18 01:45:17 +0700

    ✨ Add permission logic and example

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -6,12 +6,18 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jwt.exceptions import InvalidTokenError
 from pydantic import ValidationError
-from sqlmodel import Session
+from sqlmodel import Session, select
 
 from app.core import security
 from app.core.config import settings
 from app.core.db import engine
-from app.models import TokenPayload, User
+from app.models import (
+    Permission,
+    RolePermission,
+    TokenPayload,
+    User,
+    UserRole,
+)
 
 reusable_oauth2 = OAuth2PasswordBearer(
     tokenUrl=f"{settings.API_V1_STR}/login/access-token"
@@ -55,3 +61,26 @@ def get_current_active_superuser(current_user: CurrentUser) -> User:
             status_code=403, detail="The user doesn't have enough privileges"
         )
     return current_user
+
+
+def has_permission(session: SessionDep, user: User, permission_name: str) -> bool:
+    role_permissions = session.exec(
+        select(Permission.name)
+        .join(RolePermission)
+        .join(UserRole, RolePermission.role_id == UserRole.role_id)
+        .where(UserRole.user_id == user.id)
+    ).all()
+    if permission_name in role_permissions:
+        return True
+    return False
+
+
+def permission_required(permission_name: str):
+    def dependency(session: SessionDep, current_user: CurrentUser):
+        if not has_permission(session, current_user, permission_name):
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Insufficient permissions",
+            )
+
+    return dependency
--- backend/app/api/routes/users.py
+++ backend/app/api/routes/users.py
@@ -9,6 +9,7 @@ from app.api.deps import (
     CurrentUser,
     SessionDep,
     get_current_active_superuser,
+    permission_required,
 )
 from app.core.config import settings
 from app.core.security import get_password_hash, verify_password
@@ -38,6 +39,25 @@ router = APIRouter(prefix="/users", tags=["users"])
 def read_users(session: SessionDep, skip: int = 0, limit: int = 100) -> Any:
     """
     Retrieve users.
+    """
+
+    count_statement = select(func.count()).select_from(User)
+    count = session.exec(count_statement).one()
+
+    statement = select(User).offset(skip).limit(limit)
+    users = session.exec(statement).all()
+
+    return UsersPublic(data=users, count=count)
+
+
+@router.get(
+    "/special",
+    dependencies=[Depends(permission_required("special:read"))],
+    response_model=UsersPublic,
+)
+def read_special(session: SessionDep, skip: int = 0, limit: int = 100) -> Any:
+    """
+    Retrieve special.
     """
 
     count_statement = select(func.count()).select_from(User)
