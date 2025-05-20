44d8b6706d6b4ea34219cbbbbb48b7d732913ebe
Huda Febrianto Nurrohman <febriantohuda@gmail.com>
2025-05-20 21:45:10 +0700

    🎨 Sort router import

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -17,17 +17,17 @@ from app.api.routes import (
 from app.core.config import settings
 
 api_router = APIRouter()
-api_router.include_router(login.router)
-api_router.include_router(users.router)
-api_router.include_router(utils.router)
-api_router.include_router(items.router)
-api_router.include_router(item_categories.router)
-api_router.include_router(item_units.router)
-api_router.include_router(stores.router)
-api_router.include_router(suppliers.router)
 api_router.include_router(customer_types.router)
 api_router.include_router(customers.router)
+api_router.include_router(item_categories.router)
+api_router.include_router(item_units.router)
+api_router.include_router(items.router)
+api_router.include_router(login.router)
 api_router.include_router(permissions.router)
+api_router.include_router(stores.router)
+api_router.include_router(suppliers.router)
+api_router.include_router(users.router)
+api_router.include_router(utils.router)
 
 
 if settings.ENVIRONMENT == "local":
